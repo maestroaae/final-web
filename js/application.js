4e55a1e8d5931b49f224c81422cb71b974b0b722
maestroaae <aahmetcanov123@gmail.com>
2023-06-22 21:58:55 +0600

    adedd

--- js/application.js
+++ js/application.js
@@ -1,8 +1,8 @@
-// Selecting form and input elements
+
 const form = document.querySelector("form");
 const passwordInput = document.getElementById("password");
 const passToggleBtn = document.getElementById("pass-toggle-btn");
-// Function to display error messages
+
 const showError = (field, errorText) => {
     field.classList.add("error");
     const errorElement = document.createElement("small");
@@ -10,26 +10,26 @@ const showError = (field, errorText) => {
     errorElement.innerText = errorText;
     field.closest(".form-group").appendChild(errorElement);
 }
-// Function to handle form submission
+
 const handleFormData = (e) => {
     e.preventDefault();
-    // Retrieving input elements
+
     const fullnameInput = document.getElementById("fullname");
     const emailInput = document.getElementById("email");
     const dateInput = document.getElementById("date");
     const genderInput = document.getElementById("gender");
-    // Getting trimmed values from input fields
+
     const fullname = fullnameInput.value.trim();
     const email = emailInput.value.trim();
     const password = passwordInput.value.trim();
     const date = dateInput.value;
     const gender = genderInput.value;
-    // Regular expression pattern for email validation
+
     const emailPattern = /^[^ ]+@[^ ]+\.[a-z]{2,3}$/;
-    // Clearing previous error messages
+
     document.querySelectorAll(".form-group .error").forEach(field => field.classList.remove("error"));
     document.querySelectorAll(".error-text").forEach(errorText => errorText.remove());
-    // Performing validation checks
+
     if (fullname === "") {
         showError(fullnameInput, "Enter your full name");
     }
@@ -45,16 +45,16 @@ const handleFormData = (e) => {
     if (gender === "") {
         showError(genderInput, "Select your gender");
     }
-    // Checking for any remaining errors before form submission
+
     const errorInputs = document.querySelectorAll(".form-group .error");
     if (errorInputs.length > 0) return;
-    // Submitting the form
+
     form.submit();
 }
-// Toggling password visibility
+
 passToggleBtn.addEventListener('click', () => {
     passToggleBtn.className = passwordInput.type === "password" ? "fa-solid fa-eye-slash" : "fa-solid fa-eye";
     passwordInput.type = passwordInput.type === "password" ? "text" : "password";
 });
-// Handling form submission event
+
 form.addEventListener("submit", handleFormData);
